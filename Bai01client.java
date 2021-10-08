7090f047377ecde311837bf6d8f0d219e8b5490a
Nguyễn Quang Dũng_IT <80726405+nguyenquangdung-cntt@users.noreply.github.com>
2021-10-08 23:10:39 +0700

    Update Bai01client.java

--- Bai01client.java
+++ Bai01client.java
@@ -5,5 +5,30 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Bai01client {
+    public static void main(String[] args) throws IOException {
+        Socket mySocket = null;
+        DataOutputStream dos = null;
+        DataInputStream dis = null;
+        try {
+            mySocket = new Socket("localhost", 8000);
+            dos = new DataOutputStream(mySocket.getOutputStream());
+            dis = new DataInputStream(mySocket.getInputStream());
+            Scanner input = new Scanner(System.in);
+            String s = null;
+            while (true) {
+                System.out.println("\nNhap du lieu: ");
+                s = input.nextLine();
+                dos.writeUTF(s);
 
+                String str = dis.readUTF();
+                System.out.print("Ket qua tu Server : " + str);
+            }
+        } catch (Exception e) {
+            System.out.print("Ngat ket noi");
+            dis.close();
+            dos.close();
+            mySocket.close();
+            e.printStackTrace();
+        }
+    }
 }
